9266a6deed0ef2c9dcd7d3732e31de3020dc5060
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-17 22:00:06 +0500

    solution

--- Two Sum/Program.cs
+++ Two Sum/Program.cs
@@ -4,9 +4,30 @@ namespace Two_Sum
 {
     class Program
     {
+        static int potentialAnswer = -1;
+        /*
+         Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        You can return the answer in any order.*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] a = TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            a = TwoSum(new int[] { 3, 3 }, 6);
+            Console.WriteLine(a[0] + ", " + a[1]);
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target) return new int[] { i, j };
+                }
+            }
+            return new int[] { 0, 0 };
         }
     }
 }
